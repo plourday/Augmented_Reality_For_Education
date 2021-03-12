d210e5b0c9572295567fdf0e20417cb5e90e646d
Plourday <plourde.andrew@gmail.com>
2021-03-12 00:52:57 -0500

    Added the feature of detecting placed objects in world space

--- Development/AR Demos/Assets/Scripts/ARPlacement.cs
+++ Development/AR Demos/Assets/Scripts/ARPlacement.cs
@@ -4,60 +4,101 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 public class ARPlacement : MonoBehaviour
 {
-   public GameObject ObjectToSpawn;
-   public GameObject placementIndicator;
-   private GameObject SpawnedObject;
-   private Pose PlacementPose;
-   private ARRaycastManager raycastManager;
-   private bool placementPoseIsValid = false; 
+    [SerializeField] public GameObject objectToSpawn;
+    [SerializeField] public GameObject placementIndicator;
+    [SerializeField] public Text objectIdentifierName;
+    [SerializeField] private GameObject spawnedObject;
+    [SerializeField] private Pose placementPose;
+    [SerializeField] private ARRaycastManager raycastManager;
+    [SerializeField] private bool placementPoseIsValid = false;
+    [SerializeField] private Camera arCamera;
+
+    //Sanity Checks that there is indeed a ARRaycastManager object in the scene.
     void Start()
     {
         raycastManager = FindObjectOfType<ARRaycastManager>();
-        
+
     }
+    //Called every frame
     void Update()
     {
-        if(SpawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (spawnedObject == null)
         {
-            PlaceArObject();
+            updatePlacementPose(hits);
+            updatePlacementIndicator(hits);
         }
 
-        UpdatePlacementPose();
-        UpdatePlacementIndicator();
+
+        //Instantitate computer Object in Real World space if the placementPose Is Valid and the screen has been touched
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            placeArObject();
+        }
+        displayObjectIdentifierText();
     }
 
-    private void UpdatePlacementPose()
+    //Determines whether or not the camera is looking at a horizontal plane. If so, placement pose is valid.
+    private void updatePlacementPose(List<ARRaycastHit> hits)
     {
-            var ScreenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f,0.5f));
-            var hits = new List<ARRaycastHit>();
-            raycastManager.Raycast(ScreenCenter, hits, TrackableType.Planes);
+        Vector3 screenCenter = getScreenCenter();
 
-            placementPoseIsValid = hits.Count > 0;
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
 
-            if(placementPoseIsValid)
-            {
-                PlacementPose = hits[0].pose;
-            }
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
     }
-    
 
-    private void UpdatePlacementIndicator()
+    //sets the placement indicator to active if the placement pose is valid.
+    private void updatePlacementIndicator(List<ARRaycastHit> hits)
     {
-         if(SpawnedObject == null && placementPoseIsValid)
-         {
+        if (spawnedObject == null && placementPoseIsValid)
+        {
             placementIndicator.SetActive(true);
-            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position,PlacementPose.rotation);
-         }
-         else
-         {
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
             placementIndicator.SetActive(false);
-         }
+        }
     }
 
-    void PlaceArObject()
+    //Displays the name of the object that the camera is looking at.
+    private void displayObjectIdentifierText()
     {
-        SpawnedObject = Instantiate(ObjectToSpawn, PlacementPose.position,PlacementPose.rotation);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    PlacedObject placedObject = hitObject.transform.GetComponent<PlacedObject>();
+                    if(placedObject != null)
+                    {
+                        objectIdentifierName.text = placedObject.name.ToString();
+                    }
+                }
+            }
+        }
+    }
+    public Vector3 getScreenCenter()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        return screenCenter;
+    }
+    //Instantiates the object to be spawned in world space.
+    void placeArObject()
+    {
+        spawnedObject = Instantiate(objectToSpawn, placementPose.position, placementPose.rotation);
     }
 }
